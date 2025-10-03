015d2c80ceee1065ce4791a2b62dc9052ef920d0
Gustavo0606 <gustavovilelas22@gmail.com>
2025-10-03 13:38:16 -0300

    Alterações em Moto.java e Veiculo.java

--- Moto.java
+++ Moto.java
@@ -1,1 +1,15 @@
+public class Moto extends Veiculo{
+private int cilindradas;
+public  Moto (String placa, String marca, double preco, int cilindradas){
+    super(placa, marca, preco);
+    this.cilindradas = cilindradas;
+}
+public int getcilindradas(){
+    return cilindradas;
+}
+public int setcilindradas(){
+    return cilindradas;
+}
 
+
+}
--- Veiculo.java
+++ Veiculo.java
@@ -1,1 +1,31 @@
+public class Veiculo {
+    private String placa;
+    private String marca;
+    private double preco;
+    
+    public Veiculo (String placa, String marca, double preco){
+    this.placa = placa;
+    this.marca = marca;
+    this.preco = preco;
+    }
 
+    public String getplaca(){
+        return placa;
+    }
+    public String setplaca(){
+        return placa;
+    }
+    public String getmarca(){
+        return marca;
+    }
+    public String setmarca(){
+        return marca;
+    }
+    public double getpreco(){
+        return preco;
+    }
+    public double setpreco(){
+        return preco;
+    }
+
+}
