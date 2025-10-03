c6f0e0f484e9339067dbee89ae32840f985a2a4f
Gustavo0606 <gustavovilelas22@gmail.com>
2025-10-03 14:02:42 -0300

    Update Carro.java
    Criação de atributos, getters e setters na classe Carro

--- Carro.java
+++ Carro.java
@@ -1,1 +1,14 @@
+public class Carro extends Veiculo{
+    private int numPortas;
+    public Carro (String placa, String marca, double preco, int numPortas){
+        super(placa, marca, preco);
+        this.numPortas = numPortas;
+    }
+public int getnumPortas(){
+    return numPortas;
+}
+public void setnumPortas(int numPortas){
+    this.numPortas = numPortas;
+}
+}
 
