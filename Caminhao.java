5816833c80ec4d76b70e10a63b1d35a67e8654e9
Gustavo0606 <gustavovilelas22@gmail.com>
2025-10-03 14:03:19 -0300

    Update Caminhao.java
    criação de atributos, getters e setters na classe Caminhao

--- Caminhao.java
+++ Caminhao.java
@@ -1,1 +1,15 @@
+public class Caminhao extends Veiculo{
+    private double capacidadeCarga;
+    public Caminhao (String placa, String marca, double preco, double capacidadeCarga){
+        super(placa, marca, preco);
+        this.capacidadeCarga = capacidadeCarga;
+    }
+    public double getcapacidadeCarga(){
+        return capacidadeCarga;
+    }
+
+    public void setcapacidadeCarga(double capacidadeCarga){
+        this.capacidadeCarga = capacidadeCarga;
+    }
+}
 
